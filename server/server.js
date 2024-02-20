a1e578912ea8266546a0923e5130a5fbb4282b2f
MykelAllan <michaelallanlomugdang@gmail.com>
2024-02-20 15:36:33 -0500

    wireless

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get('/api/players/:playerTag', async (req, res) => {
         const axiosConfig = {
             headers: {
                 'Authorization': `Bearer ${apiToken}`,
-                'X-Forwarded-For': {ipAddress}
+                'X-Forwarded-For': ipAddress
             }
         };
 
